078177fe97ce9cdc53669d92c7deb657101fe623
Ambrish <ambrishabhijatya@gmail.com>
2022-10-10 16:53:07 +0530

    Updates

---
+++ precache-manifest.c3452bd4c32ace07a8bc9df17377f65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "171feb21bb8415bd41b0",
+    "url": "/TextUtils/static/css/main.a3d03755.chunk.css"
+  },
+  {
+    "revision": "171feb21bb8415bd41b0",
+    "url": "/TextUtils/static/js/main.171feb21.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/TextUtils/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "96bcc4e90747a73ef71d",
+    "url": "/TextUtils/static/js/2.96bcc4e9.chunk.js"
+  },
+  {
+    "revision": "861e73ec1b60cdc073e1",
+    "url": "/TextUtils/static/js/runtime~main.861e73ec.js"
+  },
+  {
+    "revision": "51cb10d1725eb028cad789f4d662484c",
+    "url": "/TextUtils/index.html"
+  }
+];
